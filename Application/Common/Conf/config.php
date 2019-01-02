2f80a03ca2cc9e0e4991eedba036c0733a153584
285964377 <33326597+285964377@users.noreply.github.com>
2019-01-02 13:53:10 +0800

    Update config.php

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -3,7 +3,7 @@ return array(
    	//'配置项'=>'配置值'
 	  //数据库配置信息
 	'DB_TYPE'   => 'mysql', // 数据库类型
-    'DB_HOST'   => '172.16.1.74', // 服务器地址
+    'DB_HOST'   => '', // 服务器地址
     'DB_NAME'   => 'crm', // 数据库名
     'DB_USER'   => 'root', // 用户名
     'DB_PWD'    => 'root', // 密码
